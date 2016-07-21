dab292bd06ca993f3a9dcf269f4ee20e1a12a5f2
WzxJiang <18516312500@163.com>
2016-07-21 20:28:26 +0800

    no message

--- Example/Viscosity/ViewController.swift
+++ Example/Viscosity/ViewController.swift
@@ -15,31 +15,31 @@ class ViewController: UIViewController {
         let redView = UIView()
         redView.backgroundColor = UIColor.red()
         self.view.addSubview(redView)
-        redView.vis_makeConstraints { (maker) in
-            maker.centerX.equalTo(self.view)
-            maker.centerY.equalTo(self.view)
-            maker.width.equalTo(20)
-            maker.height.equalTo(20)
+        redView.vis_makeConstraints { (make) in
+            make.centerX.equalTo(self.view)
+            make.centerY.equalTo(self.view)
+            make.width.equalTo(20)
+            make.height.equalTo(20)
         }
         
         let blueView = UIView()
         blueView.backgroundColor = UIColor.blue()
         self.view.addSubview(blueView)
-        blueView.vis_makeConstraints { (maker) in
-            maker.left.equalTo(redView.vis_right(), offset: 10)
-            maker.right.equalTo(self.view, offset: -10)
-            maker.top.equalTo(redView.vis_top())
-            maker.height.equalTo(redView)
+        blueView.vis_makeConstraints { (make) in
+            make.left.equalTo(redView.vis_right(), offset: 10)
+            make.right.equalTo(self.view, offset: -10)
+            make.top.equalTo(redView.vis_top())
+            make.height.equalTo(redView)
         }
         
         let greenView = UIView()
         greenView.backgroundColor = UIColor.green()
         self.view.addSubview(greenView)
-        greenView.vis_makeConstraints { (maker) in
-            maker.left.equalTo(self.view, offset: 10)
-            maker.right.equalTo(redView.vis_left(), offset: -10)
-            maker.top.equalTo(redView.vis_top())
-            maker.height.equalTo(redView)
+        greenView.vis_makeConstraints { (make) in
+            make.left.equalTo(self.view, offset: 10)
+            make.right.equalTo(redView.vis_left(), offset: -10)
+            make.top.equalTo(redView.vis_top())
+            make.height.equalTo(redView)
         }
         
     }
