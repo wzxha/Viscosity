3ebb3b0931ea07df059cbff027fc407e5077c6fc
wzxjiang <wzxjiang@foxmail.com>
2016-12-01 18:56:25 +0800

    no message

--- Example/Viscosity/ViewController.swift
+++ Example/Viscosity/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let label = UILabel()
+    let greenView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,29 +21,28 @@ class ViewController: UIViewController {
         label.vis_makeConstraints { (make) in
             make.left.equalTo(self.view, offset: 100)
             make.top.equalTo(self.view, offset: 100);
+            make.height.equalTo(10)
         }
 
-        let greenView = UIView()
         greenView.backgroundColor = UIColor.green
         self.view.addSubview(greenView)
         greenView.vis_makeConstraints { (make) in
             make.left.equalTo(label.vis_right, offset: 10)
             make.top.equalTo(label.vis_bottom, offset: -10)
-            make.height.equalTo(label, multiplier: 2)
+            make.height.equalTo(100)
             make.width.equalTo(label, multiplier: 1.5)
         }
         
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        label.vis_updateConstraints { (make) in
-//            make.top.equalTo(self.view, offset: 300);
-//            make.left.equalTo(self.view, offset: 300);
-//        }
-        label.vis_remakeConstraints { (make) in
-            make.right.equalTo(self.view, offset: -300);
-            make.bottom.equalTo(self.view, offset: -30);
+        label.vis_updateConstraints { (make) in
+            make.height.equalTo(greenView);
         }
+//        label.vis_remakeConstraints { (make) in
+//            make.right.equalTo(self.view, offset: -300);
+//            make.bottom.equalTo(self.view, offset: -30);
+//        }
     }
     
     override func didReceiveMemoryWarning() {
--- Viscosity.podspec
+++ Viscosity.podspec
@@ -1,11 +1,11 @@
 Pod::Spec.new do |s|
   s.name = 'Viscosity'
-  s.version = '0.0.2'
+  s.version = '0.0.3'
   s.license = 'MIT'
   s.summary = 'Harness the power of auto layout with a simplified, chainable, and compile time safe syntax.'
   s.homepage = 'https://github.com/Wzxhaha/Viscosity'
   s.authors = { 'WzxJiang' => 'wzxjiang@foxmail.com' }
-  s.source = { :git => 'https://github.com/Wzxhaha/Viscosity.git', :tag => '0.0.2' }
+  s.source = { :git => 'https://github.com/Wzxhaha/Viscosity.git', :tag => 0.0.3}
   s.platform = :ios
   s.ios.deployment_target = '8.0'
   s.source_files = 'Viscosity/*.swift'
