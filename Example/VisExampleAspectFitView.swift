b5f63005b184f7fcdf4b5fa607577f623aeb8d61
wzxjiang <wzxjiang@foxmail.com>
2017-02-10 14:47:01 +0800

    Add subscript function
    make[.left, .right, .bottom].equalTo(self)

--- Example/VisExampleAspectFitView.swift
+++ Example/VisExampleAspectFitView.swift
@@ -20,17 +20,13 @@ class VisExampleAspectFitView: UIView {
         let topView = UIView.exampleView(color: UIColor.blue.withAlphaComponent(0.5))
         self.addSubview(topView)
         topView.vis_makeConstraints { (make) in
-            make.left.equalTo(self)
-            make.right.equalTo(self)
-            make.top.equalTo(self)
+            make[.left, .right, .top].equalTo(self)
         }
         
         let bottomView = UIView.exampleView(color: UIColor.green.withAlphaComponent(0.5))
         self.addSubview(bottomView)
         bottomView.vis_makeConstraints { (make) in
-            make.left.equalTo(self)
-            make.right.equalTo(self)
-            make.bottom.equalTo(self)
+            make[.left, .right, .bottom].equalTo(self)
             make.top.equalTo(topView.vis_bottom)
             make.height.equalTo(topView)
         }
@@ -38,22 +34,18 @@ class VisExampleAspectFitView: UIView {
         let topInnerView = UIView.exampleView(color: .blue)
         topView.addSubview(topInnerView)
         topInnerView.vis_makeConstraints { (make) in
+            make.size.lessThanOrEqualTo(topView)
+            make.size.equalTo(topView).priority(UILayoutPriorityDefaultLow)
             make.width.equalTo(topInnerView.vis_height).multiplier(3)
-            make.width.lessThanOrEqualTo(topView)
-            make.height.lessThanOrEqualTo(topView)
-            make.width.equalTo(topView).priority(UILayoutPriorityDefaultLow)
-            make.height.equalTo(topView).priority(UILayoutPriorityDefaultLow)
             make.center.equalTo(topView)
         }
         
         let bottomInnerView = UIView.exampleView(color: .green)
         bottomView.addSubview(bottomInnerView)
         bottomInnerView.vis_makeConstraints { (make) in
+            make.size.lessThanOrEqualTo(bottomView)
+            make.size.equalTo(bottomView).priority(UILayoutPriorityDefaultLow)
             make.height.equalTo(bottomInnerView.vis_width).multiplier(3)
-            make.width.lessThanOrEqualTo(bottomView)
-            make.height.lessThanOrEqualTo(bottomView)
-            make.width.equalTo(bottomView).priority(UILayoutPriorityDefaultLow)
-            make.height.equalTo(bottomView).priority(UILayoutPriorityDefaultLow)
             make.center.equalTo(bottomView)
         }
     }
