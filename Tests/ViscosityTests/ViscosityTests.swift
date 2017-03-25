026a0be51ee0ec3c7bacf2645623d75fa10d0511
wzxjiang <wzxjiang@foxmail.com>
2017-03-25 13:48:36 +0800

    change tests

--- Tests/ViscosityTests/ViscosityTests.swift
+++ Tests/ViscosityTests/ViscosityTests.swift
@@ -2,6 +2,10 @@ import XCTest
 @testable import Viscosity
 
 class ViscosityTests: XCTestCase {
+    func testExample() {
+        
+    }
+    
     static var allTests : [(String, (ViscosityTests) -> () throws -> Void)] {
         return [
             ("testExample", testExample),
